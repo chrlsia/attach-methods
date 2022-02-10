9af0df243c90add4f08632886bc87da743148ce6
Chris Siannas <xrsia@otenet.gr>
2022-02-10 10:33:28 +0200

    Create and initialize instances of struct car

--- attach-methods.go
+++ attach-methods.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+type car struct {
+	color string
+	body  string
+}
+
+func main() {
+	porsche := car{color: "blue", body: "coupe"}
+	bentley := car{color: "green", body: "saloon"}
+
+	fmt.Printf("porsche is :%v\n", porsche)
+	fmt.Printf("bentley is :%v\n", bentley)
+
+}
+
+func (c car) accelerate() string {
+	return "accelerating ..."
 }
